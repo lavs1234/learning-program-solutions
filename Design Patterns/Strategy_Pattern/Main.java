6c38da9159bb6551f48a39d52160843e1e7bbccf
lavs1234 <lavanyasriram310305@gmail.com>
2025-06-19 16:32:11 +0530

    changed a line

--- Design Patterns/Strategy_Pattern/Main.java
+++ Design Patterns/Strategy_Pattern/Main.java
@@ -3,10 +3,10 @@ public class Main {
     public static void main(String[] args) {
         PaymentContext context = new PaymentContext();
 
-        context.setPaymentStrategy(new CreditCardPayment("1234-5678-9012", "Narmadha"));
+        context.setPaymentStrategy(new CreditCardPayment("1234-5678-9012", "lavanya"));
         context.payBill(1500.00);
 
-        context.setPaymentStrategy(new PayPalPayment("narmadha@example.com"));
+        context.setPaymentStrategy(new PayPalPayment("lavanya@example.com"));
         context.payBill(2200.50);
     }
 }
